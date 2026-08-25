8195f6d7797ea21315f73116fc7e1243f5fb4e83
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-25 16:15:32 +0700

    deklarasi object x sebagai binary tree,perulangan while selama nilai benar,membuat conditional dari input ch,

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -125,4 +125,60 @@ public:
 
 int main()
 {
-    
+    //deklarasi object x sebagai object BinaryTree
+    BinaryTree x;
+
+    //perulangan while selama bernilai benar
+    while (true)
+    {
+        //membuat menu program dan input pilihan
+        cout << "\nMenu" << endl;
+        cout << "1. Implement operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Enter your choice (1-5) : ";
+
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        //membuat conditional statement dari input ch
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word : ";
+            string word;
+            cin >> word;
+            x.insert(word);
+            break;
+        }
+        case '2':
+        {
+            x.inorder(x.ROOT);
+            break;
+        }
+        case '3':
+        {
+            x.preorder(x.ROOT);
+            break;
+        }
+        case '4':
+        {
+            x.postorder(x.ROOT);
+            break;
+        }
+        case '5':
+        {
+            return 0;
+        }
+        default:
+        {
+            cout << "Invalid Option" << endl;
+            break;
+        }
+        }
+    }
+}
