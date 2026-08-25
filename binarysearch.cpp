12155996a2b0381520557690f5f369f739c11ec7
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-25 15:59:02 +0700

    memberi akses modifier serta mendeklarasi variabel dan object

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 //membuat class node
 class Node {
+
+    //memberi akses modifier serta mendeklarasi variabel dan object
+    //node
+public:
+    string info;
+    Node* leftChild;
+    Node* rightChild;
+
+   
