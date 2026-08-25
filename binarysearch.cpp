86a2f3119b2fb53840a9d65858caa9d7afe7ca3a
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-25 16:09:41 +0700

    membuat conditional statement

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -45,3 +45,19 @@ public:
         Node* parent = nullptr;
         Node* currentNode = nullptr;
         search(element, parent, currentNode);
+
+        //membuat conditional statement
+        if (parent == nullptr) {
+            ROOT = newNode;
+            return;
+        }
+
+        if (element < parent->info)
+        {
+            parent->leftChild = newNode;
+        }
+        else if (element > parent->info)
+        {
+            parent->rightChild = newNode;
+        }
+    }
